21bd7191f813cf386e635d3cdc29d4fd3d4e7632
InsaneMiner <58346322+InsaneMiner@users.noreply.github.com>
2020-11-01 10:54:16 +1030

    Add files via upload

---
+++ system/applications/apps.js
@@ -0,0 +1,10 @@
+function create_new_window(width,hieght,background_color,code){
+	document.getElementById("windows").innerHTML += "<div id='divResize' style='padding:0;width: "+width+"px; height: "+hieght+"px;background-color: "+background_color+";'><div style='background-color:#5fc6ba;width:100%;margin:0;padding:0px;font-size:25px;'>&#215;</div></div>";
+}
+
+function JSbrowse(){
+
+}
+
+
+
